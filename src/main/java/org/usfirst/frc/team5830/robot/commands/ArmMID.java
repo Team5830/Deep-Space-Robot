ab8a704d643a4444befd555eb4d75208ea1f1195
nathansegura <kawalaboy12@gmail.com>
2019-02-09 16:14:57 -0500

    the arm will move to high portal

---
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/ArmMID.java
@@ -0,0 +1,39 @@
+package org.usfirst.frc.team5830.robot.commands;
+
+import org.usfirst.frc.team5830.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ArmMID extends Command {
+  public ArmMID() {
+    
+      requires(Robot.ELEVATOR);
+    }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void execute() {
+    SmartDashboard.putString("Status", "Hatch to Portal");
+     Robot.ELEVATOR.setSetpoint(4500);
+    Robot.ELEVATOR.enable();
+  }
+
+  // Called repeatedly when this Command is scheduled to run
+ 
+  
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return Math.abs (Robot.ELEVATOR.getSetpoint() - (Robot.ELEVATOR).getPosition()) < 350;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    SmartDashboard.putString("Status", "Teleop Driving");
+  }
+
+  
+}
