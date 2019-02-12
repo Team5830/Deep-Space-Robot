9590b4f017dd97affea660904ecf96aff19d8036
ZanyHunter <zanyhunter1@gmail.com>
2019-02-11 23:14:38 -0500

    Fixed broken case statements

--- src/main/java/org/usfirst/frc/team5830/robot/commands/ClimbHab.java
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/ClimbHab.java
@@ -37,94 +37,94 @@ public class ClimbHab extends InstantCommand {
     new Thread() {//This is the easiest way to get sections of code to wait without the entire robot becoming unresponsive.
       public void run() {
         switch(Robot.climbHabStepCount){
-          case 1: { //Extend side piston
+          case 1: //Extend side piston
             Robot.CYLINDERSIDE.out();
             SmartDashboard.putString("Status", "Raising Robot from Side");
             SmartDashboard.putString("Climb Next Step", "Drive Sideways");
-          } 
+            break;
           
-          case 2: {//Drive Sideways so two wheels are on platform
+          case 2: //Drive Sideways so two wheels are on platform
             Robot.swerveDrive.drive(0.15, 0, 0); //Drives Sideways
             Timer.delay(2);
             Robot.swerveDrive.drive(0, 0, 0);
             SmartDashboard.putString("Status", "Driving Sideways");
             SmartDashboard.putString("Climb Next Step", "Raise Robot from Other Side");
-          }
+            break;
 
-          case 3: {//Retracts piston near hab, extends one away from hab to level robot
+          case 3: //Retracts piston near hab, extends one away from hab to level robot
             Robot.CYLINDERSIDE.in();
             Robot.CYLINDERFRONTRIGHT.out();
             SmartDashboard.putString("Status", "Raising Robot from Side");
             SmartDashboard.putString("Climb Next Step", "Drive Onto Platform");
-          }
+            break;
 
-          case 4: {//Drives Sideways
+          case 4: //Drives Sideways
             Robot.swerveDrive.drive(0.15, 0, 0);
             Timer.delay(3);
             Robot.swerveDrive.drive(0, 0, 0);
             SmartDashboard.putString("Status", "Driving Onto Platform");
             SmartDashboard.putString("Climb Next Step", "Retract Last Cylinder");
-          }
+            break;
 
-          case 5: {//Retracts Cylinder
+          case 5: //Retracts Cylinder
             Robot.CYLINDERFRONTRIGHT.in();
             SmartDashboard.putString("Status", "Retracting Last Cylinder");
             SmartDashboard.putString("Climb Next Step", "Drive Fully Onto Platform");
-          }
+            break;
 
-          case 6: {//Drives Sideways
+          case 6: //Drives Sideways
             Robot.swerveDrive.drive(0.15, 0, 0);
             Timer.delay(3);
             Robot.swerveDrive.drive(0, 0, 0);
             SmartDashboard.putString("Status", "Driving Fully Onto Platform");
             SmartDashboard.putString("Climb Next Step", "Drive Towards Hab 3");
-          }
+            break;
 
-          case 7: {//Drives Forwards so bumpers are against hab 3
+          case 7: //Drives Forwards so bumpers are against hab 3
             Robot.swerveDrive.drive(0, 0.15, 0);
             Timer.delay(2);
             Robot.swerveDrive.drive(0, 0, 0);
             SmartDashboard.putString("Status", "Driving Towards Hab 3");
             SmartDashboard.putString("Climb Next Step", "Raise Robot from Front");
-          }
+            break;
 
-          case 8: { //Raises front of robot
+          case 8: //Raises front of robot
             Robot.CYLINDERFRONTLEFT.out();
             Robot.CYLINDERFRONTRIGHT.out();
             SmartDashboard.putString("Status", "Raising Robot from Front");
             SmartDashboard.putString("Climb Next Step", "Drive Forward");
-          }
+            break;
 
-          case 9: {//Drive forwards so two wheels are on platform
+          case 9: //Drive forwards so two wheels are on platform
             Robot.swerveDrive.drive(0, 0.15, 0);
             Timer.delay(2);
             Robot.swerveDrive.drive(0, 0, 0);
             SmartDashboard.putString("Status", "Driving Forward");
             SmartDashboard.putString("Climb Next Step", "Raise Robot from Rear");
-          }
+            break;
 
-          case 10: {//Raises rear of robot
+          case 10: //Raises rear of robot
             Robot.CYLINDERFRONTLEFT.in();
             Robot.CYLINDERFRONTRIGHT.in();
             Robot.CYLINDERREAR.out();
             SmartDashboard.putString("Status", "Raising Robot from Rear");
             SmartDashboard.putString("Climb Next Step", "Drive Onto Platform");
-          }
+            break;
 
-          case 11: {//Drive forwards so four wheels are on platform
+          case 11: //Drive forwards so four wheels are on platform
             Robot.swerveDrive.drive(0, 0.15, 0);
             Timer.delay(3);
             Robot.swerveDrive.drive(0, 0, 0);
             SmartDashboard.putString("Status", "Driving Onto Platform");
             SmartDashboard.putString("Climb Next Step", "Retract Last Piston");
-          }
+            break;
 
-          case 12: {//Retracts rear piston, robot is now fully supported by hab 3
+          case 12: //Retracts rear piston, robot is now fully supported by hab 3
             Robot.CYLINDERREAR.in();
             Robot.climbHabStepCount = -4; //Resets counter five less to prevent accidental restart
             SmartDashboard.putString("Status", "Press 5 times to restart climbing");
             SmartDashboard.putString("Climb Next Step", "NONE - End of Routine");
-          }
+            break;
         }
       }
     }.start();
