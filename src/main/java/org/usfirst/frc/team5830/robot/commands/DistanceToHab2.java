eadd2e6d76f0936ea9e85cba527fc027490617d0
HeatherKnauss <46583806+HeatherKnauss@users.noreply.github.com>
2019-02-14 22:51:24 -0500

    added distance to hab 2

--- src/main/java/org/usfirst/frc/team5830/robot/commands/DistanceToHab2.java
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/DistanceToHab2.java
@@ -3,33 +3,35 @@ package org.usfirst.frc.team5830.robot.commands;
 import org.usfirst.frc.team5830.robot.Robot;
 import org.usfirst.frc.team5830.robot.subsystems.SonicFrontLeft;
 import org.usfirst.frc.team5830.robot.subsystems.SonicFrontRight;
+import org.usfirst.frc.team5830.robot.subsystems.SonicLeftSideFront;
+import org.usfirst.frc.team5830.robot.subsystems.SonicLeftSideRear;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 
-public class DistanceToHab extends Command {
+public class DistanceToHab2 extends Command {
 
   private boolean isItFinished = false;
 
-  public DistanceToHab() {
+  public DistanceToHab2() {
   }
 
   @Override
   protected void execute() {
-    if (Math.abs(SonicFrontLeft.getDistance() - SonicFrontRight.getDistance()) > Robot.ultrasonicError){
-      while (SonicFrontLeft.getDistance() < SonicFrontRight.getDistance()){
+    if (Math.abs(SonicLeftSideFront.getDistance() - SonicLeftSideRear.getDistance()) > Robot.ultrasonicError){
+      while (SonicLeftSideFront.getDistance() < SonicLeftSideRear.getDistance()){
        Robot.swerveDrive.drive(0.15, 0, -0.15);
       }
-    while (SonicFrontLeft.getDistance() > SonicFrontRight.getDistance()){
+    while (SonicLeftSideFront.getDistance() > SonicLeftSideRear.getDistance()){
       Robot.swerveDrive.drive(-0.15, 0, 0.15);
       }
     }
 
    else {   //needs distance from hab to replace the number 5
-     if (SonicFrontLeft.getDistance() < 5){
+     if (SonicLeftSideFront.getDistance() < 5){
       Robot.swerveDrive.drive(-0.15, 0, 0);
      }
-     while (SonicFrontLeft.getDistance() > 5){
+     while (SonicLeftSideFront.getDistance() > 5){
       Robot.swerveDrive.drive(0.15, 0, 0);
      }
     }
---
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/DistanceToHab3.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team5830.robot.commands;
+
+import org.usfirst.frc.team5830.robot.Robot;
+import org.usfirst.frc.team5830.robot.subsystems.SonicFrontLeft;
+import org.usfirst.frc.team5830.robot.subsystems.SonicFrontRight;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+
+public class DistanceToHab3 extends Command {
+
+  private boolean isItFinished = false;
+
+  public DistanceToHab3() {
+  }
+
+  @Override
+  protected void execute() {
+    if (Math.abs(SonicFrontLeft.getDistance() - SonicFrontRight.getDistance()) > Robot.ultrasonicError){
+      while (SonicFrontLeft.getDistance() < SonicFrontRight.getDistance()){
+       Robot.swerveDrive.drive(0.15, 0, -0.15);
+      }
+    while (SonicFrontLeft.getDistance() > SonicFrontRight.getDistance()){
+      Robot.swerveDrive.drive(-0.15, 0, 0.15);
+      }
+    }
+
+   else {   //needs distance from hab to replace the number 5
+     if (SonicFrontLeft.getDistance() < Robot.habDistance){
+      Robot.swerveDrive.drive(-0.15, 0, 0);
+     }
+     while (SonicFrontLeft.getDistance() > 5){
+      Robot.swerveDrive.drive(0.15, 0, 0);
+     }
+    }
+     Robot.swerveDrive.drive(0, 0, 0);
+      isItFinished = true;
+    }
+
+  @Override
+  protected boolean isFinished() {
+    return isItFinished;
+  }
+}
