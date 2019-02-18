6b6484ab250ae0aca89e120a84ec1c146a5c5d41
hannahmaria9 <hannahwilson0101@yahoo.com>
2019-02-18 12:18:23 -0500

    change status

--- src/main/java/org/usfirst/frc/team5830/robot/commands/ManipulatorHighCargo.java
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/ManipulatorHighCargo.java
@@ -13,7 +13,7 @@ public class ManipulatorHighCargo extends Command {
   // Called just before this Command runs the first time
   @Override
   protected void execute() {
-    SmartDashboard.putString("Status", "Cargo to Rocket Low");
+    SmartDashboard.putString("Status", "Cargo to Rocket High");
     Robot.MANIPULATOR.setSetpoint(2500); //TODO Calibrate this number or you will kill the robot.
     Robot.MANIPULATOR.enable();
   }
