91a9f5d132c7d1f43f05ac012b5045235e5c56cf
Hunter Pugh <34586837+ZanyHunter@users.noreply.github.com>
2019-03-14 20:29:34 -0400

    Added ArmCargoShip and ArmPounce

---
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/ArmCargoShip.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team5830.robot.commands;
+
+import org.usfirst.frc.team5830.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ArmCargoShip extends Command {
+  public ArmCargoShip() {
+    requires(Robot.ARM);
+    }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void execute() {
+    SmartDashboard.putString("Arm Status", "Arm High");
+    //TODO Turn ON High DIDBoard LED
+    //TODO Turn OFF Default, Floor, Low, Mid DIDBoard LEDs
+      if(Robot.isArmAutomatic) {
+        Robot.ARM.setSetpoint(500);
+        Robot.MANIPULATOR.setSetpoint(290);
+      }
+
+    Robot.ARM.enable();}
+
+
+  
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return Math.abs (Robot.ARM.getSetpoint() - Robot.ARM.getPosition()) < 250 &&
+Math.abs (Robot.MANIPULATOR.getSetpoint() - Robot.MANIPULATOR.getPosition()) < 250;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    SmartDashboard.putString("Status", "Teleop Driving");
+  }
+}
+
+  
---
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/ArmPounce.java
@@ -0,0 +1,49 @@
+package org.usfirst.frc.team5830.robot.commands;
+
+import org.usfirst.frc.team5830.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * 
+ * @author Arlene A.
+ * 
+ */
+
+
+public class ArmPounce extends Command {
+  public ArmPounce() {
+    requires(Robot.ARM);
+    requires(Robot.MANIPULATOR);
+    }
+
+  // Called just before this Command runs the first time
+  @Override
+  protected void execute() {
+    SmartDashboard.putString("Arm Status", "Arm to Rocket Low");
+    //TODO Turn ON Low DIDBoard LED
+    //TODO Turn OFF Default, Floor, Mid, High DIDBoard LEDs
+    if(Robot.isArmAutomatic) {
+      Robot.ARM.setSetpoint(0); //TODO Calibrate this number or you will kill the robot.
+      Robot.MANIPULATOR.setSetpoint(1200);
+      Robot.ARM.enable();
+      Robot.MANIPULATOR.enable();
+    }
+  }
+
+  // Make this return true when this Command no longer needs to run execute()
+  @Override
+  protected boolean isFinished() {
+    return Math.abs (Robot.ARM.getSetpoint() - Robot.ARM.getPosition()) < 250 &&
+    Math.abs (Robot.MANIPULATOR.getSetpoint() - Robot.MANIPULATOR.getPosition()) < 250;
+  }
+
+  // Called once after isFinished returns true
+  @Override
+  protected void end() {
+    SmartDashboard.putString("Status", "Teleop Driving");
+  }
+}
+
+  
