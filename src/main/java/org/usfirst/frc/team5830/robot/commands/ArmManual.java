620db7c29a25375b5ec4332486f0f3afe8a1189f
ZanyHunter <zanyhunter1@gmail.com>
2019-03-18 21:42:57 -0400

    Cleaned up ArmManual

--- src/main/java/org/usfirst/frc/team5830/robot/commands/ArmManual.java
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/ArmManual.java
@@ -16,9 +16,6 @@ public class ArmManual extends Command {
     requires(Robot.ARM);
   }
 
-  double protectedArm = 0;
-  double protectedManip = 0;
-
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
@@ -27,51 +24,22 @@ public class ArmManual extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    //Raw throttle input (-1 to 1) needs conversion for arm setpoint. Math adds 1 to the raw joystick to give it 0 to 2.
-    //Multiplies that by half of the arm's max height so when the throttle is at full (2) the arm is at max.
-    //double valLeft = -(Robot.xbox.getY(Hand.kLeft) + 1) / 2;
-    //double valArm = MathHelper.map(valLeft, 0, 1, 0, Robot.armMaxHeight);
     Robot.armSetpointRaw = (-JoystickMappingInit.rightJoy.getRawAxis(3) + 1) * (Constants.armMaxHeight / 2);
-
-   /* if(protectedArm < Robot.xbox.getY(Hand.kLeft)){
-      protectedArm = (protectedArm + 0.1) * (Robot.armMaxHeight / 2);
-    } else if(protectedArm > Robot.xbox.getY(Hand.kLeft)) {
-      protectedArm = (protectedArm - 0.1)  * (Robot.armMaxHeight / 2);
-    }
-
-    if(protectedManip < Robot.xbox.getY(Hand.kLeft)){
-      protectedManip = protectedManip + 0.1;
-    } else if(protectedManip > Robot.xbox.getY(Hand.kLeft)) {
-      protectedManip = protectedManip - 0.1;
-    }*/
-
-    //Robot.armSetpointRaw(Robot.xbox.getY(Hand.kLeft));
-    //Robot.armSetpointRaw(protectedArm);
-    //Robot.armSetpointRaw(setpoint);
     Robot.ARM.enable();
 
-    
-
-    //double valRight = (Robot.xbox.getY(Hand.kRight) + 1) / 2;
-    //double valManip = MathHelper.map(valRight, 0, 1, 0, Robot.manipulatorMaxRotation);
     Robot.manipulatorSetpointRaw = (JoystickMappingInit.leftJoy.getRawAxis(3) + 1) * (Constants.manipulatorMaxRotation / 2);
-    //Robot.manipulatorSetpointRaw(protectedManip);
     Robot.MANIPULATOR.enable();
   }
 
-  // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return Robot.isArmAutomatic;
   }
 
-  // Called once after isFinished returns true
   @Override
   protected void end() {
   }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
   @Override
   protected void interrupted() {
   }
