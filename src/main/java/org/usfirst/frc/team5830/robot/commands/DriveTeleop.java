f0e4736995825677f3652c58853bb1aac08c58d6
Hunter Pugh <34586837+ZanyHunter@users.noreply.github.com>
2019-07-24 22:39:18 -0400

    Added automatic overcurrent protection Temporarily reduces drivetrain speed to 1/4 if total current spikes above 130 amps

--- src/main/java/org/usfirst/frc/team5830/robot/commands/DriveTeleop.java
+++ src/main/java/org/usfirst/frc/team5830/robot/commands/DriveTeleop.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team5830.robot.commands;
 
+import org.usfirst.frc.team5830.robot.Constants;
 import org.usfirst.frc.team5830.robot.Robot;
+import org.usfirst.frc.team5830.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -17,11 +19,13 @@ public class DriveTeleop extends Command {
     }
 
     protected void execute() {
-    	SmartDashboard.putString("Status", "Driving");
-    	Robot.swerveDrive.drive(Robot.driveX, Robot.driveY, Robot.rotX);
-    	
-    	//CALIBRATION ONLY
-    	//Robot.swerveDrive.drive(0, Robot.driveY, 0);
+        SmartDashboard.putString("Status", "Driving");
+        
+        if(RobotMap.pdp.getTotalCurrent() > Constants.currentLimit){ //Overcurrent trip protection
+            Robot.swerveDrive.drive(Robot.driveX/4, Robot.driveY/4, Robot.rotX/4);
+        } else {
+            Robot.swerveDrive.drive(Robot.driveX, Robot.driveY, Robot.rotX);
+        }
     }
 
     protected boolean isFinished() {
